a0ab9f5f70301d55466e57b8926828f4e05a1153
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-03-18 22:32:53 -0300

    aula sobre for

--- aula08.js
+++ aula08.js
@@ -20,8 +20,32 @@ const prompt = require("prompt-sync")();
 //executa o segundo parâmetro todo começo de ciclo e se for TRUE, ele executa o código
 //executa o terceiro parâmetro somente no final do ciclo!
 
-const filmes = ["a", "b", "c", "d"];
+// const filmes = ["a", "b", "c", "d"];
 
-for(let i = 0; i < filmes.length; i++){
-    console.log(filmes[i]);
-};
+// for(let i = 0; i < filmes.length; i++){
+//     console.log(filmes[i]);
+// };
+
+// --------- 2-
+
+// numero = +prompt("numero? ")
+// resultado = numero
+
+// for(let i = numero - 1; i > 1; i--){
+//     numero *= i
+// }
+ 
+// console.log(numero)
+
+// -------- 3-
+
+// numero = +prompt("qual o numero? ");
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(numero * (i));
+
+//     if(numero == 1 || numero == 10){
+//         console.log("mas ai estava obvio ne?...")
+//     }
+// };
+
