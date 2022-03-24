95c153764db41688cd0a44044e8a5b0fdd61eeac
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-03-23 22:30:26 -0300

    arrow function

---
+++ aula13.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")();
+
+// ---- arrow function:
+
+// funciona igualzinho a function, mas posso simplificar ela em uma linha, sem return e etc, mas posso deixa-lá mais complexa igual a function tbm!
+
+// const funcaoUm = (a, b) => a + b;
+
+// let a = 10
+// let b = 4
+
+// console.log(funcaoUm(a, b))
