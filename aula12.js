1f717459d9545120540a796314a40be01153d2d1
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-03-24 16:32:09 -0300

    exercicios da aula 12

--- aula12.js
+++ aula12.js
@@ -6,14 +6,74 @@ const prompt = require("prompt-sync")();
 
 // 1-
 
-function calc(first, second){
-    console.log('soma:', first + second);
-    console.log('subtração', first - second);
-    console.log('multiplicação', first * second);
-    console.log('divisão', first / second);
-};
+// function calc(first, second){
+//     console.log('soma:', first + second);
+//     console.log('subtração', first - second);
+//     console.log('multiplicação', first * second);
+//     console.log('divisão', first / second);
+// };
 
-let a = +prompt('primeiro valor: ');
-let b = +prompt('segundo valor: ');
+// let a = +prompt('primeiro valor: ');
+// let b = +prompt('segundo valor: ');
 
-calc(a, b);
+// calc(a, b);
+
+// 2-
+
+// function veriTres(a){
+//     return a % 3 == 0 ? true : false
+// }
+// let numero = 0;
+// do{
+// numero = +prompt('qual o numero? ')
+// }while(!(numero % 1 === 0))
+
+// numero = veriTres(numero)
+
+// console.log(numero)
+
+// 3-
+
+// function area(a, b){
+//     return a * b
+// }
+
+// const x = +prompt('largura (m): ');
+// const y = +prompt('comprimento (m): ');
+// const areaTotal = area(x, y);
+
+// console.log({areaTotal});
+
+// 4-
+
+// function distancia(x, y, z){
+//     let i = 0
+//     for( ; x < y; i++){
+//         x = x + z
+//     };
+//     return i
+// }
+// const x = +prompt('distancia inicial: ');
+// const y = +prompt('distancia final: ');
+// const z = +prompt('distancia do passo: ');
+// const total = distancia(x, y, z);
+
+// console.log(total);
+
+//5-
+
+function inverte(a){
+    return a.reverse();
+}
+
+const tamanho = +prompt('qual sera o tamanho da lista? ');
+let lista = [];
+let listaReversa = ''
+while(lista.length < tamanho){
+    let escreva = prompt('adicionar dado: ')
+    lista.push(escreva)
+}
+
+listaReversa = inverte(lista)
+
+console.log(listaReversa)
