ebf32f9b83da52af7533473b675fcb0fa7b2ab4a
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-03-25 12:31:18 -0300

    function no geral

---
+++ aula11_estudo.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")();
+
+// function pode ter parâmetros infinitos, mas não pode ter parâmetros com nomes iguais
+// parâmetros podem ser usados na função, é como se fosse uma variável dentro da função, ou seja, quando se precisa de um valor DE FORA da função para ser usado dentro da função
+
+// quando executa uma função sem return, ele retorna undefined, mas mesmo assim ELE EXECUTA TODOS OS CÓDIGOS QUE ESTÃO DENTRO DA FUNÇÃO.
+// Se executar uma função com return no final, ele executa todos os códigos da função e retorna o que estiver na frente do return.
+
+// parâmetro é o que é definido no () da construção da função, ou seja, na declaração da função nos temos os parâmetros
+// argumento é o que é definido no () quando a função é chamada, ou seja, na execução da função nos temos os argumentos.
+// não esquecendo que temos um argumento para cada parâmetro.
+// a cada execução da função, cada argumento se transforma no parâmetro correspondente! 
+
+
