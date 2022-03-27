a6ab9fd21692d0a23bbc862472d47c1e39441f12
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-03-27 00:22:46 -0300

    revisão da semana

--- aula15.js
+++ aula15.js
@@ -1,4 +1,8 @@
 const prompt = require('prompt-sync')();
 
 // lembrando que uma função pode chamar outra função!
+// e que objeto não é iterável, então não é possível percorrer seus elementos com o for of!
+
+// uma função criada dentro de um objeto é chamada de método!
+
 
